2c247f461aa4637853f27b44ccde53fad04cb07e
wongil12 <judgevi52@gmail.com>
2020-08-27 17:07:05 +0900

    스타일 추가

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './Join.css';
+
 export default function SignIn() {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
@@ -16,7 +18,7 @@ export default function SignIn() {
                     <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
                 </div>
                 <Link onClick={e => (!name || !room) ? e.preventDefault(): null} to = {`/chat?name=${name}&room=${room}`}>
-                    <button className={'button mt-20'} type="submit">Sign In</button>
+                    <button className='button' type="submit">Sign In</button>
                 </Link>
             </div>
         </div>
